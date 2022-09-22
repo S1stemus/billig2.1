3743f3162bc9ee8490804e224a69bc4887a6e868
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2022-09-23 01:55:16 +0300

    создание формы

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.Result = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.MaxNum = new System.Windows.Forms.TextBox();
+            this.MinNum = new System.Windows.Forms.TextBox();
             this.MaxCount = new System.Windows.Forms.TextBox();
             this.MinCount = new System.Windows.Forms.TextBox();
             this.MaxDiff = new System.Windows.Forms.TextBox();
@@ -47,19 +47,20 @@
             this.Result.Size = new System.Drawing.Size(399, 228);
             this.Result.TabIndex = 0;
             // 
-            // textBox2
+            // MaxNum
             // 
-            this.textBox2.Location = new System.Drawing.Point(490, 106);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 23);
-            this.textBox2.TabIndex = 1;
+            this.MaxNum.Location = new System.Drawing.Point(490, 106);
+            this.MaxNum.Name = "MaxNum";
+            this.MaxNum.Size = new System.Drawing.Size(100, 23);
+            this.MaxNum.TabIndex = 1;
+            this.MaxNum.TextChanged += new System.EventHandler(this.MaxNum_TextChanged);
             // 
-            // textBox3
+            // MinNum
             // 
-            this.textBox3.Location = new System.Drawing.Point(490, 174);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 23);
-            this.textBox3.TabIndex = 2;
+            this.MinNum.Location = new System.Drawing.Point(490, 174);
+            this.MinNum.Name = "MinNum";
+            this.MinNum.Size = new System.Drawing.Size(100, 23);
+            this.MinNum.TabIndex = 2;
             // 
             // MaxCount
             // 
@@ -93,6 +94,7 @@
             this.button1.TabIndex = 6;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -103,8 +105,8 @@
             this.Controls.Add(this.MaxDiff);
             this.Controls.Add(this.MinCount);
             this.Controls.Add(this.MaxCount);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.MinNum);
+            this.Controls.Add(this.MaxNum);
             this.Controls.Add(this.Result);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -116,8 +118,8 @@
         #endregion
 
         private System.Windows.Forms.TextBox Result;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox MaxNum;
+        private System.Windows.Forms.TextBox MinNum;
         private System.Windows.Forms.TextBox MaxCount;
         private System.Windows.Forms.TextBox MinCount;
         private System.Windows.Forms.TextBox MaxDiff;
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static биллиг1.solution;
 
 namespace WinFormsApp1
 {
@@ -17,5 +18,35 @@ namespace WinFormsApp1
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {                
+                int maxnum = int.Parse(MaxNum.Text);
+                int minnum = int.Parse(MinNum.Text);
+                if (maxnum <= 0 || minnum <= 0) throw new IndexOutOfRangeException();
+                List<int> Simp = Eratosfen(maxnum);
+                Filter(Simp, minnum);
+            }
+            catch (FormatException )
+            {
+                Result.Text = "Не туда воюешь";
+            }
+            catch (OverflowException)
+            {
+                Result.Text = "Введите число которое больше нуля";
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Result.Text = "Введите число которое больше нуля";
+            }
+
+
+        }
+
+        private void MaxNum_TextChanged(object sender, EventArgs e)
+        {
+            Result.Text = "";
+        }
     }
 }
